de0e943ac8c01aef8915d7ddf75681e62a08b69b
lav0 <andreigenuine@yahoo.com>
2018-11-02 09:19:27 +0100

    Denavit-Hartenberg parameters moved to Kinematics

--- Matrix.cpp
+++ Matrix.cpp
@@ -14,24 +14,6 @@ verybasicmath::Matrix::Matrix(const double mat[4][4])
       mMat[i][j] = mat[i][j];
 }
 
-verybasicmath::Matrix::Matrix(const DHConvention & dh)
-{
-  toIdentity();
-
-  auto ct = std::cos(dh.get_tetta());
-  auto st = std::sin(dh.get_tetta());
-  auto ca = std::cos(dh.get_alfa());
-  auto sa = std::sin(dh.get_alfa());
-
-  mMat[0][0] = ct;  mMat[0][1] = -st * ca; mMat[0][2] =  st * sa;
-  mMat[1][0] = st;  mMat[1][1] =  ct * ca; mMat[1][2] = -ct * sa;
-                    mMat[2][1] = sa;       mMat[2][2] = ca;
-  
-  mMat[0][3] = ct * dh.get_a();
-  mMat[1][3] = st * dh.get_a();
-  mMat[2][3] = dh.get_d();
-}
-
 Point verybasicmath::Matrix::operator*(const Point& pt) const
 {
   auto x = mMat[0][0] * pt[0] + mMat[0][1] * pt[1] + mMat[0][2] * pt[2] + mMat[0][3];
--- Matrix.h
+++ Matrix.h
@@ -4,29 +4,10 @@
 #include "Point.h"
 
 namespace verybasicmath {
-  struct DHConvention
-  {
-    DHConvention(double a, double alfa, double d, double tetta)
-      : link_length(a), link_twist(alfa), link_offset(d), joint_angle(tetta) {}
-    
-    double link_length;
-    double get_a() const { return link_length; }
-
-    double link_twist;
-    double get_alfa() const { return link_twist; }
-
-    double link_offset;
-    double get_d() const { return link_offset; }
-
-    double joint_angle;
-    double get_tetta() const { return joint_angle; }
-  };
-
   struct Matrix
   {
     Matrix();
     Matrix(const double[4][4]);
-    Matrix(const DHConvention&);
 
     Point operator*(const Point&) const;
     Matrix operator*(const Matrix&) const;
